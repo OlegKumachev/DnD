a3585083cecd39228fd27a1cd8582e12d1f32d32
HelegKumachev <kumachev.oleg@gmail.com>
2024-03-26 17:19:12 +0300

    init commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,30 @@
-import Game from "./game.js";
-import playGoblin from "./goblin.js";
+import List from './addCards';
+import DnD from './dragAndDrop';
 
-const game = new Game();
-playGoblin(game);
+const containers = document.querySelectorAll('.list-container');
+
+containers.forEach((container) => {
+  const list = new List(container);
+  list.bindToDOM();
+});
+
+const board = document.querySelector('.board');
+const hoverElements = document.querySelectorAll('.hover');
+const cardsVerticalDistance = 8;
+const dragAndDrop = new DnD(board, hoverElements, cardsVerticalDistance);
+
+dragAndDrop.handleMousedown();
+dragAndDrop.handleMousemove();
+dragAndDrop.handleMouseleave();
+dragAndDrop.handleMouseup();
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    const cardsList = document.activeElement.closest('.list-cards');
+    const addBtn = cardsList.querySelector('.new-card-button');
+    const closeBtn = addBtn.nextElementSibling;
+    addBtn.click();
+    closeBtn.click();
+    document.activeElement.value = '';
+  }
+});
